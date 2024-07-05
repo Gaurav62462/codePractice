9f057f16aa773de680b32f47379e699baac3dcc5
Gaurav kumar <gauravkumar@Gauravs-Air.lan>
2024-07-05 18:36:02 +0530

    worked on test task

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
 import React, { useState } from 'react';
-import Problem from './components/Problem';
-import Results from './components/Results';
-import Progress from './components/Progress';
-import { validateCode } from './services/jdoodleRestApi';
-import { problems } from './components/problems';
+import './App.css'
+import TestNav from './components/TestNav';
 
 const App = () => {
-  const [currentProblemIndex, setCurrentProblemIndex] = useState(0);
-  const [submissions, setSubmissions] = useState([]);
-  const [results, setResults] = useState([]);
 
-  const handleSubmit = async (code) => {
-       const problem = problems[currentProblemIndex];
-    const testCases = problem.testCases;
-    const result = await validateCode(code, testCases);
-    setResults((prevResults) => [...prevResults, { problem: problem.title, result }]);
-    setCurrentProblemIndex(currentProblemIndex + 1);
-  };
-
-  if (currentProblemIndex >= problems?.length) {
-    return (
-      <div>
-        <h2>Results</h2>
-        {results?.map((result, index) => (
-          <div key={index}>
-            <h3>{result.problem}</h3>
-            {result.result.map((testCase, idx) => (
-              <div key={idx}>
-                <p>Input: {testCase.input}</p>
-                <p>Expected Output: {testCase.expectedOutput}</p>
-                <p>Actual Output: {testCase.actualOutput}</p>
-                <p>Passed: {testCase.passed ? 'Yes' : 'No'}</p>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  }
 
   return (
-    <div>
-      <Progress current={currentProblemIndex} total={problems.length} />
-       <Problem problem={problems[currentProblemIndex]} onSubmit={handleSubmit} />;
-
-    </div>
+<TestNav />
   );
 };
 
